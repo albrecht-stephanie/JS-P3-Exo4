863431d5b930e961ab075c58616f73091d0798fd
AS <albrecht.stephanie@outlook.fr>
2019-10-29 14:24:40 +0100

    vérification de mot de passe en comparant 2 champs et par bouton valider

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+document.querySelector('button').addEventListener('click', function controle(){
+    var password = document.getElementById('password');
+    var confirmPassword = document.getElementById('confirmPassword');
+
+     if(password.value == confirmPassword.value){
+        password.style.borderColor='green';
+        confirmPassword.style.borderColor='green';
+    } 
+    else{
+        password.style.borderColor='red';
+        confirmPassword.style.borderColor='red';
+    }
+}); 
+
