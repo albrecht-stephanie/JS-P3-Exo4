9b955e9dd53cf8c5c8957f687ee46f42148cbed6
AS <albrecht.stephanie@outlook.fr>
2019-10-30 11:56:53 +0100

    ajout de commantaire dans script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,13 @@
-document.querySelector('button').addEventListener('click', function controle(){
-    var password = document.getElementById('password');
-    var confirmPassword = document.getElementById('confirmPassword');
+document.querySelector('button').addEventListener('click', function controle(){// applique évènement click au bouton
+    var password = document.getElementById('password');// récupère le champ password
+    var confirmPassword = document.getElementById('confirmPassword');//récupère le champ confirmPassword
 
-     if(password.value == confirmPassword.value){
+     if(password.value == confirmPassword.value){//comparaison des 2 champs, si vrai encadre les 2 champs en vert
         password.style.borderColor='green';
         confirmPassword.style.borderColor='green';
     } 
     else{
-        password.style.borderColor='red';
+        password.style.borderColor='red';//si comparaison ko, encadre les 2 champs en rouge
         confirmPassword.style.borderColor='red';
     }
 }); 
